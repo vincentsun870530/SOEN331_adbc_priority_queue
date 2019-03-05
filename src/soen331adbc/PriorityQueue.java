41f50949613ba25c774a2dc65a9811c522239860
vincent.sun870530@gmail.com <vincent.sun870530@gmail.com>
2019-03-04 19:57:48 -0500

    initial push

---
+++ src/soen331adbc/PriorityQueue.java
@@ -0,0 +1,31 @@
+package soen331adbc;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Queue;
+
+import be.ac.ua.ansymo.adbc.annotations.requires;
+
+public class PriorityQueue<E> {
+
+	public E remove(Object o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@requires ({ "E != null",
+		"$this.isFull() == false"
+		})
+	
+	public boolean add(E e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	public E min() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
